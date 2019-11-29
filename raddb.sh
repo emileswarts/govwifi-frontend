c302ad2741269845f47d51b0d919c110ea243829
Emile <emile.swarts123@gmail.com>
2019-11-29 11:09:53 +0000

    Copy authorised_macs for MAC authentication

--- raddb.sh
+++ raddb.sh
@@ -3,9 +3,12 @@
 set -o nounset -o errexit
 
 ENDPOINT_ARG=${ENDPOINT_URL:+--endpoint-url=$ENDPOINT_URL}
+RADDBDIR=/etc/raddb
 CERTSDIR=${CERTSDIR:-/etc/raddb/certs}
 
 aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/clients.conf ${CERTSDIR}/clients.conf
+echo "aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/authorised_macs ${RADDBDIR}/authorised_macs"
+aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/authorised_macs ${RADDBDIR}/authorised_macs
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ca.pem ${CERTSDIR}/ca.pem
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/comodoCA.pem ${CERTSDIR}/comodoCA.pem
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.key ${CERTSDIR}/server.key
