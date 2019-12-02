5578e9c112ea8783530af2a1a657e1bd881ef85b
Emile <emile.swarts123@gmail.com>
2019-12-02 11:43:40 +0000

    Enable MAC and Cert auth only

--- raddb.sh
+++ raddb.sh
@@ -7,7 +7,6 @@ RADDBDIR=/etc/raddb
 CERTSDIR=${CERTSDIR:-/etc/raddb/certs}
 
 aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/clients.conf ${CERTSDIR}/clients.conf
-echo "aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/authorised_macs ${RADDBDIR}/authorised_macs"
 aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/authorised_macs ${RADDBDIR}/authorised_macs
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ca.pem ${CERTSDIR}/ca.pem
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/comodoCA.pem ${CERTSDIR}/comodoCA.pem
